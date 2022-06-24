687c58f44c3191e179f62314b59b5a761249d87c
Raja Hamilton <88667222+Raja0110H@users.noreply.github.com>
2022-06-24 16:06:47 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+import { ProjectList } from "./App/ProjectList.js"
+
+class App {
+  static init() {
+    const activeProjectList = new ProjectList("active");
+    const finishedProjectList = new ProjectList("finished");
+    activeProjectList.setSwitchHandlerFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHandlerFunction(
+      activeProjectList.addProject.bind(activeProjectList)
+    );
+  }
+}
+
+App.init();
